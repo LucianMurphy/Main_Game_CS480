e4dff61cb8a9d6ecff24e968401d6da97278f0ae
Jack Lund <jlund3@uoregon.edu>
2026-04-29 14:21:33 -0700

    added basic main menu. Improve later

---
+++ Main_Game/Assets/TutorialInfo/Scripts/MainMenuController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void LoadPacMan()
+    {
+        SceneManager.LoadScene("PacMan");
+    }
+
+    public void LoadVampireHunter()
+    {
+        // not implemented yet
+    }
+
+    public void LoadSlenderMan()
+    {
+        // not implemented yet
+    }
+}
